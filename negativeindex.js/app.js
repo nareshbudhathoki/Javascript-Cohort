21b75759db53219842ca0da36f431d3ab3030892
White-Town <nareshbc5555@gmail.com>
2026-01-15 08:58:40 +0000

    finished negative indexing with proxy

--- negativeindex.js/app.js
+++ negativeindex.js/app.js
@@ -7,9 +7,21 @@ function negativeIndex(arr){
         return target[target.length + index]
       }
       return target[index]
+    },
+    set(target, prop, value){
+      const index = Number(prop);
+      if(index < 0){
+        target[target.length + index] = value;
+      }else{
+        target[index] = value;
+      }
+      return true;
     }
   })
 }
 
 const nums = negativeIndex([1,2,3,4,5]);
 console.log(nums[-1]);
+nums[-1] = 6;
+nums[1] = 0;
+console.log(nums);
